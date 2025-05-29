c218c80ef1ab3171822217ac81efa8f6bd760aa4
azime12 <azimerawtaddese75@gmail.com>
2025-05-29 16:37:55 +0300

    lksjf

--- src/services/loanApplicationService.js
+++ src/services/loanApplicationService.js
@@ -321,7 +321,6 @@ const createLoanFromApplication = async (application) => {
       
       // References
       branch_id: application.branch_id,
-      // saving_account_id: application.saving_account_id,
     });
 
     return loan;
